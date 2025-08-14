20ef351e3934a42bc3b8d3a1baacf7776a9955c2
Themiya Kasun <themiyakasun.tb@gmail.com>
2025-08-14 22:41:22 +0530

    Backend DB init

--- backend/SmartTaskManagementAPI/Program.cs
+++ backend/SmartTaskManagementAPI/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SmartTaskManagementAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
