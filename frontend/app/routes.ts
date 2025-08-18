b23376e99383529984e784af85c7aace40465445
Themiya Kasun <themiyakasun.tb@gmail.com>
2025-08-18 13:34:45 +0530

    Frontend routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,14 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from '@react-router/dev/routes';
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  layout('routes/root/root-layout.tsx', [index('routes/root/home.tsx')]),
+  layout('routes/auth/auth-layout.tsx', [
+    route('sign-in', 'routes/auth/sign-in.tsx'),
+    route('sign-up', 'routes/auth/sign-up.tsx'),
+  ]),
+] satisfies RouteConfig;
